7313ac11238f03289469effd9ec91514ff4161d6
robertlutz <48375275+robertlutz@users.noreply.github.com>
2019-03-19 11:22:13 -0700

    Version 4

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello3 CodeSandbox</h1>
+      <h1>Hello4 CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
